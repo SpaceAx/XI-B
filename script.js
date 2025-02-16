8ecf55bd16576006c856dba788de0c356c11dd59
SpaceAx <164348757+SpaceAx@users.noreply.github.com>
2025-02-16 08:38:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,19 @@ async function login() {
         message.classList.remove("hidden");
     }
 }
+
+// Fungsi untuk menampilkan dan menyembunyikan password
+function togglePassword() {
+    const passwordField = document.getElementById("password");
+    const eyeIcon = document.getElementById("eye-icon");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
